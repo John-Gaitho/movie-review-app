0c3453f667c9e9cdbc08e25315fe00bf224a6ee6
John Gaitho <jgaitho016@gmail.com>
2024-11-12 21:59:11 +0300

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-//import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation";
 import SearchMovie from "./components/SearchMovie";
 
 function App() {
-  
+  const [movies, setMovies] = useState([]); // to define the movies and setMovies with useState
 
-  
+  useEffect(() => {
+    fetch("http://localhost:5000/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
     <div>
       <Navigation />
       <SearchMovie />
+      <div>
+        {movies.map(
+          (
+            movie // to display the fetched movies
+          ) => (
+            <div key={movie.id}>
+              <h3>{movie.title}</h3>
+              <p>{movie.description}</p>
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
 }
