2acf607ff43f034cc722d593194f8c2752ff4b3b
John Gaitho <jgaitho016@gmail.com>
2024-11-12 14:46:35 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react'
+
+export class App extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+export default App
