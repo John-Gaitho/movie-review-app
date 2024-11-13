d1d47298ef69c0efadc649ed78494a8a1a1366e1
John Gaitho <jgaitho016@gmail.com>
2024-11-13 18:24:24 +0300

    details by id

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,16 +1,24 @@
 import React,{useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom';
 
 const MovieDetail = () => {
   const [movie, setMovie] = useState("");
+  const { id } = useParams();
 
   useEffect(() => {
-    fetch(`http://localhost:5000/movies/`)
+    fetch(`http://localhost:5000/movies/${id}`)
       .then(response => response.json())
       .then(data => setMovie(data));
-  }, []);
+  }, [id]);
+
+  if (!movie) return <div>Loading...</div>;//conditional rendering 
+
 
   return (
-    <div>{movie.title}</div>
+    <div>
+    <h2>{movie.title}</h2>
+    <p>Details about {movie.title}</p>
+  </div>
   )
 }
 
